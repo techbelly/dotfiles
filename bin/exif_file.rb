d3b2297c4eaca6a77977836042d5c0fc41b62d29
Ben Griffiths <bengriffiths@gmail.com>
2014-10-20 12:24:30 +0100

    Improve exif filer

--- bin/exif_file.rb
+++ bin/exif_file.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/env ruby
 
 this_dir = Dir.pwd
-files = Dir.glob("*")
+files = Dir.glob("**/*.JPG") + Dir.glob("**/*.jpg") + Dir.glob("**/*.jpeg")
 
 files.each do |file|
-  data = `exif #{file} | grep "Date and Time  "`
+  data = `exif "#{file}" | grep "Date and Time  "`
+  md5 = `md5 -q "#{file}"`.strip[0..10]
   if data != ""
     regexp = %r{.*(\d\d\d\d:\d\d:\d\d).*}
     dates = data.scan(regexp)
-    if dates.length == 1
+    if dates.length > 0
       date = dates[0]
-      `mkdir -p #{date} && mv -n #{file} #{date}`
+      dir = date
+      `mkdir -p #{date} && mv -n "#{file}" #{date}/#{md5}.jpg`
     end
   end
 end
